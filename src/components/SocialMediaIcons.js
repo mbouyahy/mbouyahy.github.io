979f52eade0edb0e8cf0c43feac9bf9dfd012995
mbouyahy <mourad.bh06@gmail.com>
2024-12-08 03:50:32 +0100

    Set up GitHub Actions for deployment to GitHub Pages

--- src/components/SocialMediaIcons.js
+++ src/components/SocialMediaIcons.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import '../styles/SocialMediaIcons.css';
 
 const SocialMediaIcons = () => {
   return (
